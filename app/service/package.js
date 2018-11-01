227b938703b37ab4e5312dfe7c184dec8f3a67aa
hongjiangyong <240093732@qq.com>
2018-11-01 15:56:28 +0800

    hh

--- app/service/package.js
+++ app/service/package.js
@@ -22,8 +22,8 @@ class PackageService extends Service {
       limit: pageSize,
       where: {
         status: 1,
-        updatedAt: { ...this.ctx.helper.whereDate({ start: updatedStart, end: updatedEnd }) },
-        ...this.ctx.helper.whereAnd({ version, status, stage, publishStatus })
+        updatedAt: { ...this.ctx.helper.whereDate({ start: updatedStart, end: updatedEnd }) }
+        // ...this.ctx.helper.whereAnd({ version, status, stage, publishStatus })
       },
       include: [
         {
