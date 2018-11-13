2d5361c627a993e81064cb5656231d0dd2d30e8c
qiangshushu <597515427@qq.com>
2018-11-13 12:05:18 +0800

    修改生产环境配置文件

--- config/config.prod.js
+++ config/config.prod.js
@@ -5,34 +5,6 @@ const path = require('path');
 module.exports = appInfo => {
   const config = (exports = {});
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1531883447101_6558';
-  config.adminPassword = 'admin';
-
-  // add your config here
-  config.middleware = ['checktoken', 'checkuser', 'checkauth'];
-  //不检查登录
-  config.checktoken = {
-    ignore: ['/login', '/package/newlist', '/product/report']
-  };
-  //不检查用户
-  config.checkuser = {
-    ignore: ['/login', '/package/newlist', '/product/report']
-  };
-  //不检查权限
-  config.checkauth = {
-    ignore: ['/login', '/package/newlist', '/product/report', '/getUserAclCodes', '/dict', '/product/all', '/getUserAclTree']
-  };
-
-  config.security = {
-    // csrf: {
-    //   headerName: 'x-csrf-token'
-    // }
-    csrf: false
-  };
-
-  config.pageSize = 10;
-
   config.sequelize = {
     dialect: 'mysql',
     host: '172.16.6.203',
@@ -44,20 +16,6 @@ module.exports = appInfo => {
     logging: false
   };
 
-  config.static = {
-    prefix: '/',
-    dir: path.join(appInfo.baseDir, 'app/public')
-  };
-
-  config.onerror = {
-    all(err, ctx) {}
-  };
-
-  config.jwt = {
-    secret: 'nova-eus-token',
-    exp: 3600 //秒
-  };
-
   config.apihost = 'http://172.16.6.203:7002';
 
   config.cluster = {
@@ -66,6 +24,10 @@ module.exports = appInfo => {
       port: 7002,
       hostname: '0.0.0.0'
     }
+  };
+
+  config.logger = {
+    consoleLevel: 'DEBUG'
   };
 
   return config;
