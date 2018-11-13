135aac1f747394d0e937894aa01b3e0f6e89d70f
qiangshushu <597515427@qq.com>
2018-11-13 11:22:25 +0800

    修改日志ip

--- app/service/syslog.js
+++ app/service/syslog.js
@@ -45,12 +45,19 @@ class SyslogService extends Service {
       operator: this.ctx.name,
       target,
       operateType,
-      ip: this.ctx.req.connection.remoteAddress,
+      ip: this.getClientIp(this.ctx.req),
       operateContent,
       createdBy: this.ctx.userId,
       updatedBy: this.ctx.userId
     });
     return log;
+  }
+
+  getClientIp(req) {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || '';
+    ip = ip.match(/\d+.\d+.\d+.\d+/);
+    ip = ip ? ip.join('.') : '';
+    return ip;
   }
 }
 
